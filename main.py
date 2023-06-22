cf0b86347370dd76abf608a75ebff03c1d4aa499
Akshar Patel <akshar8460@gmail.com>
2023-06-21 23:44:41 -0400

    Add and Diff APIs

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/sum")
+def add(a: int, b: int):
+    return {"sum": a+b}
+
+
+@app.get("/diff")
+def add(a: int, b: int):
+    return {"diff": a-b}
