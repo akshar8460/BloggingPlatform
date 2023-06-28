836f6f64067d2e9bf2b34946177b32e1807ff5a6
yours-truly-rshi <96832952+yours-truly-rshi@users.noreply.github.com>
2023-06-27 23:40:11 -0400

    Login API Developed

--- main.py
+++ main.py
@@ -1,46 +1,26 @@
-from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
-# class Item(BaseModel):
-#     name: str
-#     price: float
-#     is_offer: Union[bool, None] = None
+# class BASEMODEL_CLASS_TYPE(BaseModel):
+#     key1: type
+#     key2: type
+#     key3: type
 
-'''
+# @app.METHOD("/ENDPOINT/{PATH_PARAM}")
+# def any_name(PATH_PARAM: TYPE, QUERY_PARAMS: TYPE, BODY: BASEMODEL_CLASS_TYPE):
+#     # LOGIC
+#     return {"data": "secret"}
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+class LoginSchema(BaseModel):
+    email: str
+    password: str
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id, "item_price": item.price}
-
-
-@app.get("/sum")
-def add(a: int, b: int):
-    return {"sum": a+b}
-
-
-@app.get("/diff")
-def diff(a: int, b: int):
-    return {"diff": a-b}
-'''
-
-class
 @app.post("/login")
-def login():
-
-
+def login(login_schema: LoginSchema):
+    if login_schema.email == "admin@test.com" and login_schema.password == "admin":
+        return {"success": True}
+    return {"success": False}, 404
