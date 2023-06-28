626eda9ca9bd313e691aed7e21e1d015cd241fdc
Akshar Patel <akshar8460@gmail.com>
2023-06-27 23:30:43 -0400

    Version 1 : Starting to work

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+# class Item(BaseModel):
+#     name: str
+#     price: float
+#     is_offer: Union[bool, None] = None
+
+'''
 
 
 @app.get("/")
@@ -15,11 +24,23 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id, "item_price": item.price}
+
+
 @app.get("/sum")
 def add(a: int, b: int):
     return {"sum": a+b}
 
 
 @app.get("/diff")
-def add(a: int, b: int):
+def diff(a: int, b: int):
     return {"diff": a-b}
+'''
+
+class
+@app.post("/login")
+def login():
+
+
