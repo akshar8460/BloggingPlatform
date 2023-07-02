c9d777100004d12cd549f23cc55ac65d926346db
Akshar Patel <akshar8460@gmail.com>
2023-07-01 23:42:54 -0400

    Test file and Folder changed

---
+++ tests/test_login.py
@@ -0,0 +1,20 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+@pytest.mark.parametrize(
+    "email, password, expected_status",
+    [
+        ("invalid", None, 422),  # Test case for invalid email
+        ("admin@test.com", "admin", 200),  # Test case for successful login
+        ("admin@test.com", "323", 422),  # Test case for password length limit
+        ("admin@test.com", "admin@invalid", 401),  # Test case for 401 error
+    ]
+)
+def test_login(email, password, expected_status):
+    response = client.post("/login", json={"email": email, "password": password})
+    assert response.status_code == expected_status
