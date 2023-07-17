fe0d58bb92c58ac9a9e3d7b4b66a2739a5a70944
yours-truly-rshi <96832952+yours-truly-rshi@users.noreply.github.com>
2023-07-17 15:14:56 -0400

    Docker file fix

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from __future__ import annotations
+
 from datetime import timedelta, datetime
 from typing import List, Annotated
 
-import uvicorn
 from fastapi import FastAPI, Response, status, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
@@ -9,19 +10,22 @@ from sqlalchemy.orm import Session
 
 import crud
 import models
-from db_connector import engine, Base, get_db
+from db_connector import get_db
 from email_service_client import send_email
 from log_config import logger
 from schemas import LoginSchema, CreateAccount, CreateBlog, UpdateBlog, UpdateUser
 
-Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"  # Change as your needs
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@app.get("/api/test")
+def test():
+    return {"data": "test"}
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
@@ -151,7 +155,3 @@ def delete_blog(blog_id: int, db: Session = Depends(get_db), token_verification=
     crud.delete_blog(db, blog_id)
     response = {"success": True}
     return response
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, port=8000)
