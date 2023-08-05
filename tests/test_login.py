e33c0e0d333c3fb22008f5abc3a816f1c2cd0bf1
Akshar Patel <akshar8460@gmail.com>
2023-08-05 15:54:00 -0400

    Fix UTs

--- tests/test_login.py
+++ tests/test_login.py
@@ -10,11 +10,11 @@ client = TestClient(app)
     "email, password, expected_status",
     [
         ("invalid", None, 422),  # Test case for invalid email
-        ("admin@test.com", "admin", 200),  # Test case for successful login
+        ("admin@admin.com", "admin", 200),  # Test case for successful login
         ("admin@test.com", "323", 422),  # Test case for password length limit
         ("admin@test.com", "admin@invalid", 401),  # Test case for 401 error
     ]
 )
 def test_login(email, password, expected_status):
-    response = client.post("/login", json={"email": email, "password": password})
+    response = client.post("/api/users/login", json={"email": email, "password": password})
     assert response.status_code == expected_status
