11ee2d4dc4a8c2623e2f1bf4dfc89632dbfee30c
Akshar Patel <akshar8460@gmail.com>
2023-08-05 15:22:49 -0400

    Configuration vars set to env variables

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 import os
 
-SECRET_KEY = os.environ("SECRET_KEY", "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7")
+SECRET_KEY = os.environ.get("SECRET_KEY", "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7")
+DB_SERVER = os.environ.get("DB_SERVER", "localhost")
+DB_USER = os.environ.get("DB_USER", "postgres")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "admin")
+DB_NAME = os.environ.get("DB_NAME", "blogging")
--- db_connector.py
+++ db_connector.py
@@ -3,9 +3,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import *
 
 # Connection string / URL
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost:5432/blogging"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}:5432/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
