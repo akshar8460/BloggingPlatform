b7b1ed087c34a5c4785e4bb2f4eb4e013459fc88
Akshar Patel <akshar8460@gmail.com>
2023-08-07 15:21:24 -0400

    Test User Flow Conslidated UT

--- tests/test_login.py
+++ tests/test_login.py
@@ -15,16 +15,24 @@ def test_login(client, initialize_sample_data, email, password, expected_status)
     assert response.status_code == expected_status
 
 
-def test_register_user(client, initialize_sample_data):
+def test_user_flow(client, initialize_sample_data, jwt_header):
     response = client.post("/api/users/register",
                            json={"name": "new", "email": "new@new.com", "password": "password"})
     assert response.status_code == 201
+
+    user_id = response.json()["id"]
+
+    response = client.put(f"/api/users/{user_id}", json={"name": "new2", "email": "new@new.com", "password": "password"}, headers=jwt_header)
+    assert response.status_code == 200
+    assert response.json()["name"] == "new2"
 
     response = client.post("/api/users/register",
                            json={"name": "new", "email": "new@new.com", "password": "password"})
     assert response.status_code == 403
 
     response = client.post("/api/users/login", json={"email": "new@new.com", "password": "password"})
+    assert response.status_code == 200
+    response = client.delete(f"/api/users/{user_id}", headers=jwt_header)
     assert response.status_code == 200
 
 
@@ -37,30 +45,15 @@ def test_get_all_users(client, initialize_sample_data, jwt_header):
 def test_get_user(client, initialize_sample_data, jwt_header):
     response = client.get("/api/users/1", headers=jwt_header)
     assert response.status_code == 200
+    assert response.json()["name"] == "john_doe"
 
 
-# @pytest.mark.parametrize(
-#     "id, email, name, password, expected_status",
-#     [
-#         (1, "invalid", None, None, 422),  # Test case for invalid email
-#         (3, "admin@test.com", None, "admin", 400),  # Test case for invalid id
-#         (1, "admin@test.com", None, "admin", 400),  # Test case for missing parameter
-#         (1, "admin@test.com", "New", "323", 422),  # Test case for password length limit
-#         (1, "admin@test.com", "new", "admin@invalid", 401),  # Test case for 401 error
-#         (1, "admin@test.com", "test1", "admin", 200)  # Test case for successful update operation
-#     ]
-# )
-# def test_update_user(client, initialize_sample_data, jwt_header, id, email, name, password, expected_status):
-#     response = client.post(f"/api/users/{id}", headers=jwt_header)
-#     assert response.status_code == expected_status
-
-
-def test_delete_user(client, initialize_sample_data, jwt_header):
-    response = client.delete("/api/users/1", headers=jwt_header)
-    assert response.status_code == 200
+def test_create_blog(client, initialize_sample_data, jwt_header):
+    response = client.post("/api/blogs",
+                           json={"topic": "new", "data": "This a new blog created"}, headers=jwt_header)
+    assert response.status_code == 201
 
 
 def test_get_all_blogs(client, initialize_sample_data, jwt_header):
     response = client.get("/api/blogs", headers=jwt_header)
     assert response.status_code == 200
-    assert len(response.json()) == 0
