432a2c6f923c5d66d89a93630c2dc8b8fc981844
Akshar Patel <akshar8460@gmail.com>
2023-08-07 01:16:19 -0400

    Unit test with testing database

---
+++ tests/conftest.py
@@ -0,0 +1,51 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from config import *
+from db_connector import Base, get_db
+from main import app
+from models import User
+
+client = TestClient(app)
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_SERVER}:5432/{DB_TEST_NAME}"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture(scope="session")
+def db():
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    yield db
+    db.close()
+    Base.metadata.drop_all(bind=engine)
+
+
+@pytest.fixture(scope="module")
+def client(db):
+    def override_get_db():
+        try:
+            db = TestingSessionLocal()
+            yield db
+        finally:
+            db.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    client = TestClient(app)
+    yield client
+
+
+@pytest.fixture(scope="module")
+def initialize_sample_data(request, db):
+    if not hasattr(request.module, "_sample_data_initialized"):
+        test_data = {"name": "john_doe", "email": "test@test.com", "password": "admin"}
+        # Create a User instance and add it to the session
+        user = User(**test_data)
+        db.add(user)
+        db.commit()
+        request.module._sample_data_initialized = True
+    yield
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,20 +1,29 @@
 import pytest
-from fastapi.testclient import TestClient
-
-from main import app
-
-client = TestClient(app)
 
 
 @pytest.mark.parametrize(
     "email, password, expected_status",
     [
         ("invalid", None, 422),  # Test case for invalid email
-        ("admin@admin.com", "admin", 200),  # Test case for successful login
+        ("test@test.com", "admin", 200),  # Test case for successful login
         ("admin@test.com", "323", 422),  # Test case for password length limit
         ("admin@test.com", "admin@invalid", 401),  # Test case for 401 error
     ]
 )
-def test_login(email, password, expected_status):
+def test_login(client, initialize_sample_data, email, password, expected_status):
     response = client.post("/api/users/login", json={"email": email, "password": password})
     assert response.status_code == expected_status
+
+
+def test_register_user(client, initialize_sample_data):
+    response = client.post("/api/users/login", json={"email": "test@test.com", "password": "admin"})
+    JWT_TOKEN = response.json()["token"]
+    headers = {
+        "Authorization": f"Bearer {JWT_TOKEN}"
+    }
+    response = client.post("/api/users/register",
+                           json={"name": "new", "email": "new@new.com", "password": "password"},
+                           headers=headers)
+    assert response.status_code == 201
+    response = client.post("/api/users/login", json={"email": "new@new.com", "password": "password"})
+    assert response.status_code == 200
